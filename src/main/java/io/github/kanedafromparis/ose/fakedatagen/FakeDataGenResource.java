e5f8c24f20bacd4c7f94ca876e444d3df8e0702f
Charles Sabourdin <kanedafromparis@gmail.com>
2016-12-14 20:55:24 +0100

    install files

--- src/main/java/io/github/kanedafromparis/ose/fakedatagen/FakeDataGenResource.java
+++ src/main/java/io/github/kanedafromparis/ose/fakedatagen/FakeDataGenResource.java
@@ -7,17 +7,42 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
+@Path("data") 
 @RequestScoped
-@Path("/data")
+
 public class FakeDataGenResource {
-    @Path("/data/v1")
+
+    private final ExecutorService executorService = java.util.concurrent.Executors.newCachedThreadPool();
+
+    /**
+     *
+     * @param asyncResponse
+     * @param deep
+     */
+    @Path("v1")    
     @GET()
     @Produces("application/json")
-    public HashMap<String, String> getData(@QueryParam("deep") Integer deep
-    ) {
-        HashMap<String, String> theInsult = new HashMap<>();
-        theInsult.put("insult", new FakeDataGenerator().generateFakeData());
-        return theInsult;
+    public void getData(@Suspended
+    final AsyncResponse asyncResponse, @QueryParam(value = "deep")
+    final Integer deep) {
+        executorService.submit(() -> {
+            asyncResponse.resume(doGetData(deep));
+        });
     }
+
+    private HashMap<String, List<FDGCompany>> doGetData(@QueryParam("deep") Integer deep) {
+        HashMap<String, List<FDGCompany>> companies = new HashMap<>();
+        FakeDataGenerator fakeDataGenerator = new FakeDataGenerator();     
+        List<FDGCompany> companiesLst = fakeDataGenerator.generateFakeData(5,6);
+        String buffer = fakeDataGenerator.toJSon(companiesLst);
+        companies.put("companies", companiesLst);
+        return companies;
+    }
+
+    
 }
--- src/main/java/io/github/kanedafromparis/ose/fakedatagen/FakeDataGenerator.java
+++ src/main/java/io/github/kanedafromparis/ose/fakedatagen/FakeDataGenerator.java
@@ -7,54 +7,58 @@ import io.codearte.jfairy.producer.person.Address;
 import io.codearte.jfairy.producer.person.Person;
 import org.apache.commons.lang3.RandomUtils;
 
-import org.json.simple.JSONObject;
-
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class FakeDataGenerator {
 
-    public String generateFakeData() {
-        return this.generateFakeData(10);
+    public List<FDGCompany> generateFakeData() {
+        return this.generateFakeData(10,5);
     }
 
-    public String generateFakeData(Integer deep) {
-        StringBuilder sb = new StringBuilder();
-        Fairy fairy = Fairy.create();
+    public List<FDGCompany> generateFakeData(Integer deepCompany, Integer topRangePersons) {
+        Fairy fairy = Fairy.create(Locale.FRANCE);
+        
         Calendar start = Calendar.getInstance();
 
-        JSONObject jsoReq = new JSONObject();
-        JSONArray jsoCompanies = new JSONArray();
-        jsoReq.put("companies", jsoCompanies);
-        while (deep > 0) {
-            deep = deep - 1;
-            Integer nbPersonnes = RandomUtils.nextInt(0, 5);
+        List<FDGCompany> lstCompany = new ArrayList();
+        while (deepCompany > 0) {
+            deepCompany = deepCompany - 1;
+            Integer nbPersonnes = RandomUtils.nextInt(1, topRangePersons);
             Integer nbPersonnesComp = nbPersonnes;
             System.out.println(MessageFormat.format("Creating {0} user", nbPersonnesComp));
 
             Company company = fairy.company();
-            JSONObject jsoCompany = new JSONObject();
-            jsoCompanies.add(jsoCompany);
-            String annotation = fairy.textProducer().sentence(RandomUtils.nextInt(5, 25));
-            jsoCompany.put("companyName", company.name());
-            jsoCompany.put("annotation", annotation);
+            FDGCompany oCompany = new FDGCompany();
+            lstCompany.add(oCompany);
 
-            JSONArray jsoPersons = new JSONArray();
-            jsoCompany.put("persons", jsoPersons);
+            String annotation = fairy.textProducer().sentence(RandomUtils.nextInt(5, 25));
+            oCompany.setName(company.name());
+            oCompany.setNote(annotation);
+
+            List<FDGPersonne> t = new ArrayList();
+            oCompany.setFKPersonneLst(t);
             while (nbPersonnes > 0) {
                 nbPersonnes = nbPersonnes - 1;
+                FDGPersonne oPerson = new FDGPersonne();
+                t.add(oPerson);
+
                 Person person = fairy.person();
-                JSONObject jsoPerson = new JSONObject();
+
                 String firstname = person.firstName();
                 String lastname = person.lastName();
-                jsoPerson.put("firstname", firstname);
-                jsoPerson.put("lastname", lastname);
+                oPerson.setFirstname(firstname);
+                oPerson.setLastname(lastname);
 
                 CreditCard card = fairy.creditCard();
                 String cardVendor = card.vendor();
-
-                jsoPerson.put("cardVendor", cardVendor);
+                oPerson.setCardVendor(cardVendor);
 
                 Address adress = person.getAddress();
 
@@ -63,11 +67,11 @@ public class FakeDataGenerator {
                 String postalCode = adress.getPostalCode();
                 String city = adress.getCity();
 
-                jsoPerson.put("stnum", stnum);
-                jsoPerson.put("street", street);
-                jsoPerson.put("postalCode", postalCode);
-                jsoPerson.put("city", city);
-                jsoPersons.add(jsoPerson);
+                oPerson.setStnum(stnum);
+                oPerson.setStreet(street);
+                oPerson.setPostalCode(postalCode);
+                oPerson.setCity(city);
+
             }
 
         }
@@ -75,8 +79,41 @@ public class FakeDataGenerator {
         Calendar end = Calendar.getInstance();
         long delta = end.getTimeInMillis() - start.getTimeInMillis();
         System.out.println(MessageFormat.format("Process took {0} Milliseconds", delta));
-        return jsoReq.toJSONString();
+        fairy = null;
+        return lstCompany;
 
     }
 
+    String toJSon(List<FDGCompany> compLst) {
+        JSONArray jsoCompanies = new JSONArray();
+        Iterator<FDGCompany> iterator = compLst.iterator();
+        while (iterator.hasNext()) {
+            FDGCompany next = iterator.next();
+            JSONObject jsoCompany = new JSONObject();
+            jsoCompanies.add(jsoCompany);
+            jsoCompany.put("companyName", next.getName());
+            jsoCompany.put("annotation", next.getNote());
+
+            JSONArray jsoPersons = new JSONArray();
+            jsoCompany.put("persons", jsoPersons);
+
+            Iterator<FDGPersonne> iterator1 = next.getFKPersonneLst().iterator();
+            while (iterator1.hasNext()) {
+                FDGPersonne person = iterator1.next();
+                JSONObject jsoPerson = new JSONObject();
+                jsoPersons.add(jsoPerson);
+                jsoPerson.put("firstname", person.getFirstname());
+                jsoPerson.put("lastname", person.getLastname());
+                jsoPerson.put("cardVendor", person.getCardVendor());
+                jsoPerson.put("stnum", person.getStnum());
+                jsoPerson.put("street", person.getStreet());
+                jsoPerson.put("postalCode", person.getPostalCode());
+                jsoPerson.put("city", person.getCity());
+                jsoPersons.add(jsoPerson);
+
+            }
+
+        }
+        return jsoCompanies.toJSONString();
+    }
 }
